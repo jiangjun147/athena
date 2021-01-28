f383e18acd3e2b220b9e6b43fe5a4aab977a6243
rickone <rickone@sina.com>
2021-01-28 14:50:18 +0800

    fix: ws

--- ginex/ws.go
+++ ginex/ws.go
@@ -3,12 +3,13 @@ package ginex
 import (
 	"context"
 	"encoding/json"
+	"log"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
-	"github.com/rickone/athena/common"
+	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -25,80 +26,143 @@ var (
 	}
 )
 
+type WebSocket struct {
+	conn   *websocket.Conn
+	read   chan *WSMessage
+	write  chan *WSMessage
+	ctx    context.Context
+	cancel context.CancelFunc
+}
+
 type WSMessage struct {
 	Type int
 	Data []byte
 }
 
-func JSONWSMessage(val interface{}) *WSMessage {
-	data, err := json.Marshal(val)
-	common.AssertError(err)
+func NewWebSocket(c *gin.Context) (*WebSocket, error) {
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		return nil, err
+	}
 
-	return &WSMessage{
-		Type: websocket.TextMessage,
+	read := make(chan *WSMessage)
+	write := make(chan *WSMessage)
+
+	conn.SetReadLimit(readLimit)
+	conn.SetReadDeadline(time.Now().Add(healthCheckInterval))
+	conn.SetPingHandler(func(data string) error {
+		write <- &WSMessage{
+			Type: websocket.PongMessage,
+		}
+		return nil
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	ws := &WebSocket{
+		conn:   conn,
+		read:   read,
+		write:  write,
+		ctx:    ctx,
+		cancel: cancel,
+	}
+
+	go ws.doRead()
+	go ws.doWrite()
+	return ws, nil
+}
+
+func (ws *WebSocket) doRead() {
+	log.Println("doRead begin")
+	defer log.Println("doRead end")
+
+	for {
+		msgType, data, err := ws.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				logrus.Errorf("ws.ReadMessage err: %v", err)
+			}
+			break
+		}
+		ws.conn.SetReadDeadline(time.Now().Add(healthCheckInterval))
+
+		if msgType == websocket.TextMessage || msgType == websocket.BinaryMessage {
+			ws.read <- &WSMessage{
+				Type: msgType,
+				Data: data,
+			}
+		}
+	}
+	ws.Cancel()
+}
+
+func (ws *WebSocket) doWrite() {
+	log.Println("doWrite begin")
+	defer log.Println("doWrite end")
+
+	for {
+		select {
+		case msg := <-ws.write:
+			err := ws.conn.WriteMessage(msg.Type, msg.Data)
+			if err != nil {
+				if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+					logrus.Errorf("ws.WriteMessage err: %v", err)
+				}
+				return
+			}
+		case <-ws.ctx.Done():
+			return
+		}
+	}
+}
+
+func (ws *WebSocket) Read() <-chan *WSMessage {
+	return ws.read
+}
+
+func (ws *WebSocket) Write(msgType int, data []byte) {
+	ws.write <- &WSMessage{
+		Type: msgType,
 		Data: data,
 	}
 }
 
-func WebSocket(f func(ctx context.Context, c *gin.Context, out chan *WSMessage) error) gin.HandlerFunc {
+func (ws *WebSocket) WriteJSON(val interface{}) error {
+	data, err := json.Marshal(val)
+	if err != nil {
+		return err
+	}
+
+	ws.Write(websocket.TextMessage, data)
+	return nil
+}
+
+func (ws *WebSocket) Cancel() {
+	ws.cancel()
+}
+
+func (ws *WebSocket) Close() {
+	ws.conn.Close()
+}
+
+func (ws *WebSocket) Context() context.Context {
+	return ws.ctx
+}
+
+func WSWrap(f func(c *gin.Context, ws *WebSocket) error) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+		ws, err := NewWebSocket(c)
 		if err != nil {
 			c.AbortWithStatus(http.StatusNotFound)
 			return
 		}
-		defer conn.Close()
+		defer ws.Close()
 
-		out := make(chan *WSMessage)
-		defer close(out)
-
-		conn.SetReadLimit(readLimit)
-		conn.SetReadDeadline(time.Now().Add(healthCheckInterval))
-		conn.SetPingHandler(func(data string) error {
-			out <- &WSMessage{
-				Type: websocket.PongMessage,
-			}
-			return nil
-		})
-
-		ctx, cancel := context.WithCancel(context.Background())
-
-		go func() {
-			for {
-				select {
-				case msg := <-out:
-					err := conn.WriteMessage(msg.Type, msg.Data)
-					if err != nil {
-						if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-							GetLogger(c).Errorf("ws.WriteMessage err: %v", err)
-						}
-						return
-					}
-				case <-ctx.Done():
-					return
-				}
-			}
-		}()
-
-		go func() {
-			for {
-				_, _, err := conn.ReadMessage()
-				if err != nil {
-					if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-						GetLogger(c).Errorf("ws.ReadMessage err: %v", err)
-					}
-					break
-				}
-				conn.SetReadDeadline(time.Now().Add(healthCheckInterval))
-			}
-			cancel()
-		}()
-
-		err = f(ctx, c, out)
+		err = f(c, ws)
 		if err != nil {
 			if s, ok := status.FromError(err); !ok || s.Code() != codes.Canceled {
-				GetLogger(c).Errorf("ws.Handler err: %v", err)
+				GetLogger(c).Errorf("WSWrap err: %v", err)
 			}
 		}
+		ws.Cancel()
 	}
 }
