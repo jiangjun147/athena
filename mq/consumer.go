1cb78ab87a963e51954c2d8486abfb054b0f84d1
rickone <rickone@sina.com>
2021-04-02 16:19:30 +0800

    feat: mq logs

--- mq/consumer.go
+++ mq/consumer.go
@@ -2,15 +2,19 @@ package mq
 
 import (
 	"context"
-	"fmt"
+	"encoding/base64"
+	"log"
+	"runtime/debug"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/nsqio/go-nsq"
 	"github.com/rickone/athena/common"
 	"github.com/rickone/athena/config"
+	"github.com/rickone/athena/errcode"
 	"github.com/rickone/athena/grpcex"
 	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc/status"
 )
 
 type Consumer struct {
@@ -33,21 +37,45 @@ func (c *Consumer) OnWithTimeout(topic string, f func(ctx context.Context, m *ns
 	common.AssertError(err)
 
 	consumer.AddHandler(nsq.HandlerFunc(func(m *nsq.Message) (err error) {
-		defer func() {
-			if err != nil {
-				logrus.WithFields(logrus.Fields{
-					"topic":  topic,
-					"msg_id": fmt.Sprintf("0x%x", m.ID),
-					"err":    err.Error(),
-				}).Error("Consume failed")
-			}
-		}()
-
 		ctx, cancel := context.WithTimeout(context.Background(), timeout)
 		defer cancel()
 
 		reqId := uuid.New().String()
 		ctx = grpcex.NewCtxWithValue(ctx, c.channel, "", "", reqId, "", "", topic)
+		start := time.Now()
+
+		defer func() {
+			latency := time.Now().Sub(start).Milliseconds()
+
+			fields := map[string]interface{}{
+				"body":    base64.StdEncoding.EncodeToString(m.Body),
+				"latency": latency,
+			}
+
+			if err != nil {
+				fields["err"] = err.Error()
+				grpcex.GetLogger(ctx).WithFields(fields).Error("Consume failed")
+			} else {
+				grpcex.GetLogger(ctx).WithFields(fields).Error("Consume success")
+			}
+		}()
+
+		defer func() {
+			if ret := recover(); ret != nil {
+				stack := string(debug.Stack())
+				grpcex.GetLogger(ctx).WithFields(logrus.Fields{
+					"stack": stack,
+					"err":   ret,
+				}).Error("Recover panic")
+				log.Printf("panic: %v\n%s\n", ret, stack)
+
+				if retErr, ok := ret.(error); ok {
+					err = retErr
+				} else {
+					err = status.Errorf(errcode.ErrRpcPanic, "recover panic: %v", ret)
+				}
+			}
+		}()
 
 		err = f(ctx, m)
 		return
